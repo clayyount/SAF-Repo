10e6a8ef07d96b2b3cd9afb621432970c0eff482
clayyount <clayyount@gmail.com>
2012-03-14 18:40:33 -0400

    prelim start screen

--- js/canvassmooth.js
+++ js/canvassmooth.js
@@ -126,6 +126,10 @@ $(document).ready(function(){
 	
 })
 $(window).load(function(){
+$("#startscreen").show();
+}
+function startGame(){
+$("#startscreen").hide();
 	//Check for Wacom plugin and write the plugin object if it is installed
 	for(i=0;i<navigator.plugins.length;i++){
 		if(navigator.plugins[i].name=="WacomTabletPlugin" && navigator.plugins[i].length>=1){
