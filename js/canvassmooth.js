175ed6fbae8a1e5e16860586bb5577f54c591d12
clayyount <clayyount@gmail.com>
2012-03-14 21:25:58 -0400

    added Facebook login button

--- js/canvassmooth.js
+++ js/canvassmooth.js
@@ -12,6 +12,28 @@ function preload(arrayOfImages) {
         // (new Image()).src = this;
     });
 }
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '239015749524254', // App ID
+      oauth		 : true,
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+    // Additional initialization code here
+  };
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+
+
 preload(["images/wheelofdeathbg.png","images/cursor_size_1.cur","images/cursor_size_2.cur","images/cursor_size_3.cur","images/cursor_size_4.cur","images/cursor_size_5.cur","images/cursor_size_6.cur","images/cursor_hand.cur","images/cursor_size_6.cur"])
 
 var commandStack = [];
