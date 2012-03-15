344032aad9ceb14085cc9b8a6e9fc70944c27102
clayyount <clayyount@gmail.com>
2012-03-15 01:44:20 -0400

    fb

--- js/canvassmooth.js
+++ js/canvassmooth.js
@@ -215,7 +215,7 @@ $(window).load(function(){
 			for(var j=0;j<friendList.length;j++){
 				if(data[i].players.indexOf(friendList[j])!=-1){
 					gameList.push(data[i])
-					gameListHtml+='<a onclick=\'function(){joinGame(\''+data[i].gameID+'\')};\'>'+data[i].gameID+'</a>'
+					gameListHtml+='<a onclick=\'function(){joinGame(\''+data[i].gameID+'\')};\'>'+friendList[j]+'</a>'
 				}
 			}
 		}
