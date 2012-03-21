5928f717564d5585b971767675ba1479498087e9
clayyount <clayyount@gmail.com>
2012-03-21 15:53:10 -0400

    update

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ window.fbAsyncInit = function() {
     });
 	
 	FB.getLoginStatus(fblogin,true);
-	//FB.Event.subscribe('auth.login',fblogin);
+	FB.Event.subscribe('auth.login',fblogin);
 	//FB.Event.subscribe('auth.statusChange',fblogin);
 	
 };
