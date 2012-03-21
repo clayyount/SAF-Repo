3cdb895e146d0dc0fd6de267fe7066abde203857
clayyount <clayyount@gmail.com>
2012-03-21 18:29:12 -0400

    update

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ function checkForWacom(){
 		}
 	}
 	if(pluginInstalled){
-		$("body").append('<object style="visiblity:hidden;" id="wtPlugin" type="application/x-wacomtabletplugin" classid="CLSID:092dfa86-5807-5a94-bf3b-5a53ba9e5308" codebase="fbWacomTabletPlugin.cab" width="0" height="0"> <param name="onload" value="pluginloaded" /></object>');
+		$("body").append('<object style="visiblity:hidden;" id="wtPlugin" type="application/x-wacomtabletplugin" classid="CLSID:092dfa86-5807-5a94-bf3b-5a53ba9e5308" codebase="fbWacomTabletPlugin.cab" width="0" height="0"> <param name="onload" value="pluginLoaded" /></object>');
 	}
 	penAPI = plugin().penAPI;
 	debug("penAPI")
@@ -110,6 +110,11 @@ function checkForWacom(){
 
 	//END Check Wacom plugin
 }
+
+function pluginLoaded(){
+	debug("Plugin loaded!");
+}
+
 
 // Load the SDK Asynchronously
 $(document).ready(function(){
