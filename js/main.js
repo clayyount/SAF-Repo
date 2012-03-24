9e7d292eff386467375e2673bb5f8699700ecc12
clayyount <clayyount@gmail.com>
2012-03-23 21:21:32 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1046,21 +1046,21 @@ if(obj.s=="1"){
 	var lsmY=obj.lsmY
 	var smX=obj.smX
 	var smY=obj.smY
-	debug("thing")
-	debug(obj.s)
+	
 	if(obj.ld=="1"){
 		//brush stroke is starting, draw a straight line
 		debug("!line down");
 		canto(canvasid).beginPath().moveTo(lsmX*redrawMultiplier,lsmY*redrawMultiplier).lineTo(smX*redrawMultiplier,obj.smY*redrawMultiplier).stroke({lineWidth: lineW, lineCap:"none", strokeStyle: "rgba("+ obj.bc.r+", "+ obj.bc.g+", "+ obj.bc.b+", "+ brushAlpha+")"}).endPath();
 	}else{
 	//if (false) {
-		if (obj.vX*obj.lvX + obj.vY*obj.lvY < 0 || obj.lvc) {	
+		if (obj.vX*obj.lvX + obj.vY*obj.lvY < 0 || obj.lvc=="1") {	
 			//quick change of velocity, draw a straight line
 			canto(canvasid).beginPath().moveTo(lsmX*redrawMultiplier,lsmY*redrawMultiplier).lineTo(smX*redrawMultiplier, obj.smY*redrawMultiplier).stroke({lineWidth: lineW, lineCap:"round", strokeStyle: "rgba("+ obj.bc.r+", "+ obj.bc.g+", "+ obj.bc.b+", "+ brushAlpha+")"}).endPath();
 			
 			canto(canvasid).beginPath().moveTo(obj.psX*redrawMultiplier,obj.psY*redrawMultiplier).lineTo(lsmX*redrawMultiplier, lsmY*redrawMultiplier).stroke({lineWidth: lineW, lineCap:"round", strokeStyle: "rgba("+ obj.bc.r+", "+ obj.bc.g+", "+ obj.bc.b+", "+ brushAlpha+")"}).endPath();
 			
 		}else{
+			debug("curve")
 			//draw a curved line with stroke
 			canto(canvasid).beginPath().moveTo((lsmX)* redrawMultiplier, (lsmY)* redrawMultiplier).quadraticCurveTo(controlX* redrawMultiplier,controlY* redrawMultiplier,(smX )*redrawMultiplier, (obj.smY)* redrawMultiplier).stroke({lineWidth:lineW, lineCap:"round", strokeStyle: "rgba("+ obj.bc.r+", "+ obj.bc.g+", "+ obj.bc.b+", "+ brushAlpha+")"}).endPath();
 	
