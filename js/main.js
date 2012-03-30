f660f3016390f9d46bb2dd141d12545188d1c2e1
clayyount <clayyount@gmail.com>
2012-03-30 17:05:06 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1049,7 +1049,6 @@ if(!dragging){
 }
 
 function drawSmoothLine(obj){
-	debug(obj)
 if(obj.lsmX){
 	var redrawMultiplier=(canvasFactor/parseFloat(obj.cf));
 	canvasid="canvas"
@@ -1124,7 +1123,15 @@ canto(canvasid).beginPath().moveTo(lsmX*redrawMultiplier,lsmY*redrawMultiplier).
 			ctx.strokeStyle="rgba("+ obj.bc.r+", "+ obj.bc.g+", "+ obj.bc.b+", "+ brushAlpha+")";
 			ctx.lineCap="round";
 			ctx.stroke();
-			ctx.closePath()
+			ctx.closePath();
+			debug("ctx.beginPath()");
+			debug("ctx.moveTo("+((lsmX)* redrawMultiplier)+","+((lsmY)* redrawMultiplier)+")")
+			debug("ctx.quadraticCurveTo("+(controlX* redrawMultiplier)+","+(controlY* redrawMultiplier)+","+((smX )*redrawMultiplier)+","+ ((obj.smY)* redrawMultiplier)+")")
+			debug("ctx.lineWidth="+lineW);
+			debug('ctx.strokeStyle="black"'
+			debug('ctx.lineCap="round"');
+			debug('ctx.stroke()');
+			debug('ctx.closePath()');
 			/*
 			}else{
 			ctx.beginPath();
