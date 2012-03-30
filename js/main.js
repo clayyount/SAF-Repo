377c1776fc5e42f449087cc2b4cfe2b9a9a81466
clayyount <clayyount@gmail.com>
2012-03-30 12:13:35 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1131,6 +1131,8 @@ canto(canvasid).beginPath().moveTo(lsmX*redrawMultiplier,lsmY*redrawMultiplier).
 	}	
 }else{
 	debug("smoothing off")
+	debug(parseFloat(obj.lsmX))
+	debug(parseFloat(obj.lsmY))
 var lineW=(parseFloat(obj.bs)*redrawMultiplier * parseFloat(obj.p))
 canto(canvasid).beginPath().moveTo(parseFloat(obj.lsmX)*redrawMultiplier,parseFloat(obj.lsmY)*redrawMultiplier).lineTo(parseFloat(obj.smX)*redrawMultiplier,parseFloat(obj.smY)*redrawMultiplier).stroke({lineWidth: (parseFloat(obj.bs)*redrawMultiplier * parseFloat(obj.p)), lineCap:"round", strokeStyle: "rgba("+ obj.bc.r+", "+ obj.bc.g+", "+ obj.bc.b+", "+ brushAlpha+")"}).endPath();
 }
