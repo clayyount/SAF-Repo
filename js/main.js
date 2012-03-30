df8bfb41af9a5a509a82c376aa86c6408a895b8a
clayyount <clayyount@gmail.com>
2012-03-30 12:47:33 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1117,7 +1117,7 @@ canto(canvasid).beginPath().moveTo(lsmX*redrawMultiplier,lsmY*redrawMultiplier).
 		}else{
 			debug("curved line")
 			//draw a curved line with stroke
-			canto(canvasid).beginPath().moveTo((lsmX)* redrawMultiplier, (lsmY)* redrawMultiplier).quadraticCurveTo(controlX* redrawMultiplier,controlY* redrawMultiplier,(smX )*redrawMultiplier, (obj.smY)* redrawMultiplier).stroke({lineWidth:lineW, lineCap:"round", strokeStyle: "red"}).closePath()
+			canto(canvasid).moveTo((lsmX)* redrawMultiplier, (lsmY)* redrawMultiplier).beginPath().quadraticCurveTo(controlX* redrawMultiplier,controlY* redrawMultiplier,(smX )*redrawMultiplier, (obj.smY)* redrawMultiplier).closePath().stroke({lineWidth:lineW, lineCap:"round", strokeStyle: "red"}).endPath();
 	
 	//just in case I can't figure out the overlapping...
 	//.stroke({lineWidth:0, lineCap:"none", strokeStyle: "rgba("+ obj.bc.r+", "+ obj.bc.g+", "+ obj.bc.b+", "+ brushAlpha+")"})
