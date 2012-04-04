ff691b5d2c0086252d9b2e8d23dcb7b84a6ebd8b
clayyount <clayyount@gmail.com>
2012-04-04 15:17:16 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1094,11 +1094,11 @@ if(parseFloat(obj.s)){
 	//controlY1 = obj.lsmY + L0Sin0 + controlVecY;
 	//controlX2 = obj.lsmX - L0Cos0 + controlVecX;
 	//controlY2 = obj.lsmY - L0Sin0 + controlVecY;
-	var lineW=roundNumber((parseFloat(obj.bs)*redrawMultiplier * parseFloat(obj.p)),0)
-	var lsmX=parseInt(obj.lsmX)
-	var lsmY=parseInt(obj.lsmY)
-	var smX=parseInt(obj.smX)
-	var smY=parseInt(obj.smY)
+	var lineW=(parseFloat(obj.bs)*redrawMultiplier * parseFloat(obj.p))
+	var lsmX=parseFloat(obj.lsmX)
+	var lsmY=parseFloat(obj.lsmY)
+	var smX=parseFloat(obj.smX)
+	var smY=parseFloat(obj.smY)
 	
 	if(parseFloat(obj.ld)){
 		//brush stroke is starting, draw a straight line
