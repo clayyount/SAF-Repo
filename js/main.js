09f70cbc1f0707467a64b1aee2afd32e88eda3aa
Clay Yount <clayyount@gmail.com>
2012-11-08 01:40:48 -0500

    again

--- js/main.js
+++ js/main.js
@@ -67,10 +67,7 @@ function debug(message){
 		console.log(message);
 	}
 }
-function getWacomPlugin()
-{
-	return document.getElementById('wtPlugin');
-}
+
 function isPluginLoaded()
     	{
 		var retVersion = "";
