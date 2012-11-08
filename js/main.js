a3d928f3e19cc87320fddcff7d8426f5b6fdf652
Clay Yount <clayyount@gmail.com>
2012-11-08 14:37:44 -0500

    update

--- js/main.js
+++ js/main.js
@@ -264,12 +264,26 @@ $('#mainmenu').live('pageinit',function(event){
 	//set the default transitions to fade because mobile safari screws up canvas drawing if 3d transitions are applied.
 	$.mobile.defaultDialogTransition="fade"
 	$.mobile.defaultPageTransition="fade"
-	splashScreenH=$(window).height()-30;
-	splashScreenW=$(window).width()-30;
+	splashScreenH=$(window).height();
+	splashScreenW=$(window).width();
 	splashH=700
 	splashW=700
 	//$("#splash_content").css({marginTop:(splashScreenH/2-(splashH/2)),marginLeft:(splashScreenW/2-(splashW/2))})
-	$("#splash_buttonholder").css({"margin-top":"388px"})
+	
+	if(splashScreenH>=641){
+		$("#splash_buttonholder").css({"margin-top":"388px"})
+		$("#splashpage").css({"background":"url(../images/saf_logo.png) no-repeat center 20px;"})
+		//full size splashscreem
+	}else if(splashScreenH<641 && splashScreenH>=484 ){
+		$("#splash_buttonholder").css({"margin-top":"236px"})
+		$("#splashpage").css({"background":"url(../images/saf_logo_mid.png) no-repeat center 20px;"})
+		//mid size splashscreem
+	}
+	else if(splashScreenH<484){
+		$("#splash_buttonholder").css({"margin-top":"70px"})
+		$("#splashpage").css({"background":"url(../images/saf_logo_small.png) no-repeat center 20px;"})
+		//small size splashscreem
+	}
 });
 
 //on drawing page init
