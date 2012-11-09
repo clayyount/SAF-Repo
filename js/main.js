076379f9ff7675c1f60b1d23cbd252cf888fac96
Clay Yount <clayyount@gmail.com>
2012-11-09 16:02:39 -0500

    update

--- js/main.js
+++ js/main.js
@@ -633,6 +633,11 @@ function newGame(){
 	var friendListHTML='<ul id="friendList" data-role="listview" data-theme="a">';
 	friendsOnline.forEach(function(friend){
 		friendListHTML+='<li><a href="#"><img src="http://graph.facebook.com/'+friend.userID+'/picture?type=square" />'+friend.screenname+'</a></li>'
+		friendListHTML+='<li><a href="#"><img src="http://graph.facebook.com/'+friend.userID+'/picture?type=square" />'+friend.screenname+'</a></li>'
+		friendListHTML+='<li><a href="#"><img src="http://graph.facebook.com/'+friend.userID+'/picture?type=square" />'+friend.screenname+'</a></li>'
+		friendListHTML+='<li><a href="#"><img src="http://graph.facebook.com/'+friend.userID+'/picture?type=square" />'+friend.screenname+'</a></li>'
+		friendListHTML+='<li><a href="#"><img src="http://graph.facebook.com/'+friend.userID+'/picture?type=square" />'+friend.screenname+'</a></li>'
+
 	})
 	friendListHTML+='</ul>'
 	$("#friendCollapsable p").html(friendListHTML);
