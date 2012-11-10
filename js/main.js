3e3a879e04afbf2acaf985343cc107f3cd01beae
Clay Yount <clayyount@gmail.com>
2012-11-10 12:38:45 -0500

    update

--- js/main.js
+++ js/main.js
@@ -347,7 +347,7 @@ checkForWacom()
  	
  	debug("drawing page initiated")
 	//Load Wheel of Death
-	//loadWheelofDeath()
+	loadWheelofDeath()
 	//set the default brush and marker buttons to selected state
 	$("#brush_size_2").css({backgroundPosition: "top right"})
 	$("#marker_black").css({backgroundPosition: "top right"})
